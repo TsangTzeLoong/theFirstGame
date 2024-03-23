ce4ce399f1d60ef1fe95ac48c4683bb9dd1779b4
TzeLoong <jeremytsang722@gmail.com>
2024-03-23 23:49:32 +0800

    first commit

---
+++ Assets/Script/MenuController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class MenuController : MonoBehaviour{
+
+[SerializeField]
+private Button _startBtn;
+[SerializeField]
+private Button _exitBtn;
+[SerializeField]
+private GameObject _exitGameConfirmUI;
+[SerializeField]
+private Button _yesBtn;
+[SerializeField]
+private Button _noBtn;
+    void Start(){
+        _startBtn.onClick.AddListener(StartGame);
+        _exitBtn.onClick.AddListener(CloseGamePop);
+        _yesBtn.onClick.AddListener(ExitGame);
+        _noBtn.onClick.AddListener(CloseGameCancel);
+    }
+
+    private void StartGame() {
+        Debug.Log("game started");
+    }
+    private void CloseGamePop(){
+        _exitGameConfirmUI.SetActive(true);
+    }
+    private void CloseGameCancel(){
+        _exitGameConfirmUI.SetActive(false);
+    }
+    private void ExitGame(){
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false; // 在编辑器模式下停止播放
+        #else
+            Application.Quit(); // 在发布模式下退出应用程序
+        #endif
+    }
+}
